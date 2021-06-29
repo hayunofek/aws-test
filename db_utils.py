0c1edbce015c1f528a698a119a2f8239080253fa
ofek hayun <hayunofek@gmail.com>
2021-06-29 20:39:13 +0300

    added untested db_utils, to test postgres connection

---
+++ db_utils.py
@@ -0,0 +1,25 @@
+import psycopg2
+
+try:
+    connection = psycopg2.connect(
+            user = os.environ['DB_USER'],
+            password = os.environ['DB_PASSWORD'],
+            host = os.environ['DB_HOST'],
+            port = os.environ['DB_PORT'],
+            database = os.environ['DB_DB'],
+    )
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print(f"You are connect into the - {record}")
+except (Exception, psycopg2.Error) as error:
+    print("Error connection to PostgreSQL database", error)
+    connection = None
+
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is now closed")
