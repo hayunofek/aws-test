7ea4817a79b9335c7495dc6fcccf992870d82658
ofek hayun <hayunofek@gmail.com>
2021-06-30 23:07:19 +0300

    inserted stubs for all endpoints

--- app/book_manager.py
+++ app/book_manager.py
@@ -1,31 +1,39 @@
 import os
 
 from flask import Flask
-from db_utils import *
-
+import db_utils as db
 
 app = Flask(__name__)
 
-@app.route("/books", methods = ['GET'])
-def get_all_books():
-    pass
 
-@app.route("/books/<book_id>", methods = ['GET'])
+@app.route("/books", methods=['GET'])
+def get_all_books():
+    return "get_all_books"
+
+
+@app.route("/books/<book_id>", methods=['GET'])
 def get_book(book_id):
-    pass
+    return "get_book"
+
 
 # POST
-@app.route("/books", methods = ['POST'])
+@app.route("/books", methods=['POST'])
 def add_book():
     data = request.form
-    pass
+    return "post books"
+
 
 # PUT
-@app.route("/books/<book_id>", methods = ['PUT'])
+@app.route("/books/<book_id>", methods=['PUT'])
 def edit_book(book_id):
-    pass
+    return "put book"
+
 
 # DELETE
-@app.route("/books/<book_id>", methods = ['DELETE'])
+@app.route("/books/<book_id>", methods=['DELETE'])
 def delete_book(book_id):
-    pass
+    return "put delete"
+
+@app.route("/")
+def delete_book(book_id):
+    return "ok"
