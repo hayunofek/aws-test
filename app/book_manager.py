ec2aa9f39e64249d315eddf22e398b540741416a
ofek hayun <hayunofek@gmail.com>
2021-07-01 12:02:02 +0300

    implemented CRUD functions of book_manager

--- app/book_manager.py
+++ app/book_manager.py
@@ -1,41 +1,158 @@
-from flask import Flask, request
-import db_utils as db
+from flask import Flask, request, Response
+import traceback
+import json
+import utils as utils
 
-connection, cursor = db.open_connection_and_cursor()
-db.init_db(cursor)
-db.close_connection(connection, cursor)
+utils.startup_db()
 
 app = Flask(__name__)
 
 
 @app.route("/books", methods=['GET'])
 def get_all_books():
-    return "get_all_books"
+    """
+    This function returns all books
+    :return: Response object
+    """
+    connection, cursor = None, None
+    try:
+        connection, cursor = utils.open_connection_and_cursor()
+        books = utils.get_all_books_from_db(cursor)
+        return Response(json.dumps({"status": "ok", "message": books}), status=200, mimetype='application/json')
+    except (Exception, utils.psycopg2.Error) as e:
+        print(f"An excepct occured, exception: {e}")
+        connection = None
+        return Response(json.dumps({"status": "error", "message": "an error has occured"}), status=500,
+                        mimetype="application/json")
+    finally:
+        if connection:
+            utils.close_connection(connection, cursor)
 
 
 @app.route("/books/<book_id>", methods=['GET'])
 def get_book(book_id):
-    return "get_book"
+    """
+    This function retrieves a book by its id
+    :param book_id: int
+    :return: Response object
+    """
+    connection, cursor = None, None
+    try:
+        connection, cursor = utils.open_connection_and_cursor()
+        if not book_id.isnumeric():
+            return Response(json.dumps({"status": "error", "message": "book_id needs to be a number"}), status=400,
+                            mimetype="application/json")
+        book_object = utils.get_book_from_db(cursor, book_id)
+        return Response(json.dumps({"status": "ok", "message": book_object}), status=200, mimetype="application/json")
+    except (Exception, utils.psycopg2.Error) as e:
+        print(f"An excepct occured, exception: {e}")
+        connection = None
+        return Response(json.dumps({"status": "error", "message": "an error has occured"}), status=500,
+                        mimetype="application/json")
+    finally:
+        if connection:
+            utils.close_connection(connection, cursor)
 
 
 # POST
 @app.route("/books", methods=['POST'])
 def add_book():
-    data = request.form
-    return "post books"
+    """
+    This function adds a book to the database given a book object over a POST request
+    book object should look like this: {'name': str, 'price': str}
+    :return: Response object
+    """
+    connection, cursor = None, None
+    try:
+        connection, cursor = utils.open_connection_and_cursor()
+        data = request.json
+        print(data)
+        if not utils.is_book_object_valid(data):
+            return Response(json.dumps({"status": "error",
+                                        "message": "book_object sent is invalid. It should look like this: {'price': "
+                                                   "str, "
+                                                   "'name': str}"}),
+                            status=400, mimetype="application/json")
+        book_id = utils.add_book_to_db(cursor, data)
+        return Response(json.dumps({"status": "ok", "message": f"Book id {book_id} was added successfully"}),
+                        status=200,
+                        mimetype="application/json")
+    except (Exception, utils.psycopg2.Error) as e:
+        print(f"An excepct occured, exception: {e}")
+        connection = None
+        return Response(json.dumps({"status": "error", "message": "an error has occured"}), status=500,
+                        mimetype="application/json")
+    finally:
+        if connection:
+            utils.close_connection(connection, cursor)
 
 
-# PUT
-@app.route("/books/<book_id>", methods=['PUT'])
-def edit_book(book_id):
-    return "put book"
+@app.route("/books", methods=['PUT'])
+def edit_book():
+    """
+    This function edits a book given a book object over PUT
+    book object should look like this: {'name': str, 'price': str, 'ID': int}
+    :return: Response object
+    """
+    connection, cursor = None, None
+    try:
+        connection, cursor = utils.open_connection_and_cursor()
+        data = request.json
+        print(data)
+        if not utils.is_book_object_valid(data, should_contain_id=True):
+            return Response(json.dumps({"status": "error",
+                                        "message": "book_object sent is invalid. It should look like this: {'ID': "
+                                                   "int, 'price': "
+                                                   "str, 'name': str}"}),
+                            status=400, mimetype="application/json")
+        book_id = utils.edit_book(cursor, data)
+        if book_id:
+            return Response(json.dumps({"status": "ok", "message": f"Book id {book_id} was edited successfully"}),
+                            status=200,
+                            mimetype="application/json")
+        return Response(json.dumps({"status": "ok", "message": f"Book id {data['ID']} doesn't exist"}),
+                        status=200,
+                        mimetype="application/json")
+    except (Exception, utils.psycopg2.Error) as e:
+        traceback.print_exc()
+        print(f"An excepct occured, exception: {e}")
+        connection = None
+        return Response(json.dumps({"status": "error", "message": "an error has occured"}), status=500,
+                        mimetype="application/json")
+    finally:
+        if connection:
+            utils.close_connection(connection, cursor)
 
 
-# DELETE
 @app.route("/books/<book_id>", methods=['DELETE'])
 def delete_book(book_id):
-    return "put delete"
+    """
+    This function deletes a book given a book_id
+    :param book_id: integer
+    :return: Response object
+    """
+    connection, cursor = None, None
+    try:
+        connection, cursor = utils.open_connection_and_cursor()
+        if not book_id.isnumeric():
+            return Response(json.dumps({"status": "error", "message": "book_id needs to a number"}),
+                            status=400, mimetype="application/json")
+        utils.delete_book_from_db(cursor, book_id)
+        return Response(json.dumps({"status": "ok", "message": "Book removed successfully"}), status=200,
+                        mimetype="application/json")
+    except (Exception, utils.psycopg2.Error) as e:
+        print(f"An excepct occured, exception: {e}")
+        connection = None
+        return Response(json.dumps({"status": "error", "message": "an error has occured"}), status=500,
+                        mimetype="application/json")
+    finally:
+        if connection:
+            utils.close_connection(connection, cursor)
+
 
 @app.route("/")
 def health_check():
+    """
+    This function is an endpoint for checking the health of the application
+    """
     return "ok"
---
+++ app/utils.py
@@ -0,0 +1,109 @@
+import psycopg2
+import os
+
+
+def open_connection_and_cursor():
+    connection, cursor = None, None
+
+    try:
+        connection = psycopg2.connect(
+            user=os.environ['DB_USER'],
+            password=os.environ['DB_PASSWORD'],
+            host=os.environ['DB_HOST'],
+            port=os.environ['DB_PORT'],
+        )
+        connection.set_session(autocommit=True)
+        cursor = connection.cursor()
+    except (Exception, psycopg2.Error) as error:
+        print("Error connection to PostgreSQL database", error)
+        connection = None
+
+    return connection, cursor
+
+
+def close_connection(connection, cursor):
+    if connection:
+        connection.commit()
+        if cursor:
+            cursor.close()
+        connection.close()
+
+
+def add_book_to_db(cursor, book_object):
+    """
+    This function adds a book to the database, this function assumes book_object is valid
+    :param cursor: PostgreSQL connection cursor
+    :param book_object: A book object dictionary without ID {'name': str, 'price': str}
+    :return: ID (int) of book
+    """
+    sql_query = "INSERT INTO books(name, price) VALUES (%s, %s) RETURNING ID"
+    cursor.execute(sql_query, (book_object['name'], book_object['price']))
+    result = cursor.fetchone()[0]
+    return result
+
+
+def edit_book(cursor, book_object):
+    """
+    This function edits a book in the database, this function assumes book_object is valid
+    :param cursor: PostgreSQL connection cursor
+    :param book_object: A book object dictionary with ID {'name': str, 'price': str, 'ID': int}
+    :return: ID (int) of book
+    """
+    sql_query = f"UPDATE books SET name = %s, price = %s WHERE ID = {book_object['ID']} RETURNING ID"
+    cursor.execute(sql_query, (book_object['name'], book_object['price']))
+    result = cursor.fetchone()
+    if result:
+        result = result[0]
+    return result
+
+
+def get_book_from_db(cursor, book_id):
+    cursor.execute(f"SELECT ID, name, price FROM books WHERE ID={book_id}")
+    result = cursor.fetchone()
+    result_dict = {}
+    if result:
+        result_dict['ID'] = result[0]
+        result_dict['name'] = result[1]
+        result_dict['price'] = result[2]
+    return result_dict
+
+
+def init_db(cursor):
+    cursor.execute("""CREATE TABLE IF NOT EXISTS books (ID INT GENERATED ALWAYS AS IDENTITY, name VARCHAR NOT NULL, 
+    price VARCHAR NOT NULL, PRIMARY KEY (ID)) """)
+
+
+def startup_db():
+    connection, cursor = open_connection_and_cursor()
+    init_db(cursor)
+    close_connection(connection, cursor)
+
+
+def get_all_books_from_db(cursor):
+    cursor.execute("SELECT ID, name, price FROM books")
+    results = cursor.fetchall()
+    results_json = []
+    for result in results:
+        results_json.append({'ID': result[0],
+                             'name': result[1],
+                             'price': result[2]})
+
+    return results_json
+
+
+def delete_book_from_db(cursor, book_id):
+    print(book_id)
+    print(type(book_id))
+    sql_query = f"DELETE FROM books WHERE ID = {book_id}"
+    cursor.execute(sql_query)
+
+
+def is_book_object_valid(book_object, should_contain_id=False):
+    print(book_object)
+    is_valid = True
+    if should_contain_id:
+        is_valid = is_valid and 'ID' in book_object and isinstance(book_object['ID'], int)
+    is_valid = is_valid and 'name' in book_object and isinstance(book_object['name'], str)
+    is_valid = is_valid and 'price' in book_object and isinstance(book_object['price'], str)
+    print(is_valid)
+    return is_valid
